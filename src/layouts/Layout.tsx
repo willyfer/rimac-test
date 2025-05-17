d7b55dd25b35b7b2bdec2f9e276c42863def7af7
Willy Fernandez <desarrollador.frontend@navitrack.com.pe>
2025-05-17 11:09:31 -0500

    fix: footer 3/3

--- src/layouts/Layout.tsx
+++ src/layouts/Layout.tsx
@@ -22,7 +22,7 @@ const Layout: React.FC = () => {
           </div>
         </div>
       </header>
-      <main className='m-0 p-0 bg-[#FAFBFF] h-[87vh] '>
+      <main className='m-0 p-0 bg-[#FAFBFF] h-min-fit h-[90vh] '>
         <Outlet />
       </main>
     </div>
