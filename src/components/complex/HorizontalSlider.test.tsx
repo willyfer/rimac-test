fe659ad7e59c4454e42c2f204b2dc29de2d36c03
Willy Fernandez <desarrollador.frontend@navitrack.com.pe>
2025-05-17 02:15:08 -0500

    feat: tests

--- src/components/complex/HorizontalSlider.test.tsx
+++ src/components/complex/HorizontalSlider.test.tsx
@@ -1,6 +1,6 @@
 import { render, screen } from '@testing-library/react'
-import userEvent from '@testing-library/user-event'
-import { describe, it, expect, vi } from 'vitest'
+
+import { describe, it, expect } from 'vitest'
 import HorizontalSlider from './HorizontalSlider'
 
 describe('HorizontalSlider component', () => {
